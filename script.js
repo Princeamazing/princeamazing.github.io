e2450b4bc769c8e762f1fe853c3c2f62638d8920
Princeamazing <karantaneja9@outlook.com>
2026-06-02 13:47:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const card = document.getElementById("profile");
+roblox_card = document.getElementById("roblox_profile");
 
-card.addEventListener("click", () => {
+roblox_card.addEventListener("click", () => {
     window.open("https://www.roblox.com/users/1933051665/profile", "_blank");
+});
+
+gd_card = document.getElementById("gd")
+
+gd_card.addEventListener("click", () => {
+    navigator.clipboard.writeText("NotGamerboyXD")
+    alert("Copied to clipboard! ")
 });
